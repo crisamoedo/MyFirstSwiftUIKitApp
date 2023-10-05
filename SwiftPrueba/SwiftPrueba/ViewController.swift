b5b8112a46b66ed081a07888718df90df490889f
Cristina Amoedo <gizanakris@gmail.com>
2023-10-05 16:07:43 +0200

    Primeras pruebas insertando un label y un botón y añadiendo constraints

--- SwiftPrueba/SwiftPrueba/ViewController.swift
+++ SwiftPrueba/SwiftPrueba/ViewController.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var userLabel: UILabel!
+    
+    @IBOutlet weak var suscribeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        userLabel.textColor = .blue
+        
     }
 
-
+    @IBAction func didTapOnSuscribeButton(_ sender: Any) {
+        print("El usuario se ha suscrito a tu canal")
+        
+    }
+    
 }
 
